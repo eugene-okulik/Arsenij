fb6e77d2b3f8d435f961a8d90f39b2f94c2c2650
ArseniySko <arsdigitalgroup@gmail.com>
2026-02-12 23:04:50 +0300

    Добавил 4 задание

---
+++ homework/Arseney_Skomorovsky/Homework_4/Lesson_4.py
@@ -0,0 +1,14 @@
+my_dict = {}
+my_dict['tuple'] = (1,2,3,4,5,7)
+my_dict['list'] =[1,2,3,4,5]
+my_dict['set'] = {1,2,3,4,5}
+my_dict['dict'] = {'1':'true','2':'False','3':'pythone','4':'5','5':'5'}
+
+print((my_dict['tuple'])[-1])
+my_dict['list'].append(17)
+my_dict['list'].pop(1)
+my_dict['dict'] ['i am a tuple'] ='surpeise'
+my_dict['dict'].pop('3')
+my_dict['set'].add(69)
+my_dict['set'].pop()
+print(my_dict)
