e2ecef5431062c524e62158002163b0746ff6f9e
ArseniySko <arsdigitalgroup@gmail.com>
2026-02-13 17:10:15 +0300

    [Добавил кортеж в словарь 11 строка]

--- homework/Arseney_Skomorovsky/Homework_4/Lesson_4.py
+++ homework/Arseney_Skomorovsky/Homework_4/Lesson_4.py
@@ -8,8 +8,8 @@ my_dict['dict'] = {'1': 'true', '2': 'False', '3': 'pythone', '4': '5', '5': '5'
 print(my_dict['tuple'][-1])
 my_dict['list'].append(17)
 my_dict['list'].pop(1)
-my_dict['dict']['i am a tuple'] = 'surpeise'  # исправлено: 'surpeise' на 'surprise'? (опечатка)
-my_dict['dict'].pop('3')
+my_dict['dict'][('i am a tuple',)] = 'surprise'
+my_dict['dict'].pop('1')
 my_dict['set'].add(69)
 my_dict['set'].pop()
 print(my_dict)
